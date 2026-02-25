e3e96fe6e7842e27def2c8be49292dec3e965ff4
Cyber Angel <cyber@mj00.top>
2026-02-25 17:59:30 +1300

    quickfix

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys
 
-sys.path.insert(0, '/home/dl.170045.xyz/public_html')
+sys.path.insert(0, '/home/dl.170045.xyz/xyz170045')
 
 from xyz170045 import app as application
