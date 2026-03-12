cf10b979bc989d4798cacc9224501c4c4c7c4845
Cyber Angel <cyber@mj00.top>
2026-03-12 21:28:41 +1300

    wsgi update

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,4 @@ import sys
 
 sys.path.insert(0, '/home/dl.170045.xyz/xyz170045')
 
-from xyz170045 import create_app
-
-# Create the Flask application
-app = create_app()
-
-# Expose "application" for WSGI servers like uWSGI, Gunicorn, mod_wsgi
-application = app
-
+from xyz170045 import app as application
