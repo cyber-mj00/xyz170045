766bb92d0c08597b62bcfb674c2facccae99417e
Cyber Angel <cyber@mj00.top>
2026-03-12 21:26:29 +1300

    rollback flask

---
+++ xyz170045/task.py
@@ -0,0 +1,14 @@
+from getdata import *
+from getsheet import *
+
+def main():
+    dayaya, majsoul, player_pool, team_pool, game_pool = loadContestData()
+    generateSheet(player_pool, team_pool, game_pool)
+    print(f"[{Dayaya.getCurrentTime()}] Scheduled task: updated.")
+
+def generateSheet(player_pool, team_pool, game_pool):
+    df_players, df_players_byteam, df_games, team_data = getDf(player_pool, team_pool, game_pool)
+    getSheet(player_pool, team_pool, game_pool, df_players, df_players_byteam, df_games, team_data)
+
+if __name__ == "__main__":
+    main()
